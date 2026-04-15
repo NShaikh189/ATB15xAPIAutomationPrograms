39b8e5a66d68c433a459d3afd33aaf33357e8b3c
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-04-15 23:40:47 +0530

    API automation

---
+++ src/test/java/org/thetestingacademy/ex03_RestAssured_testNG_Advance/POST_BDDStyle.java
@@ -0,0 +1,5 @@
+package org.thetestingacademy.ex03_RestAssured_testNG_Advance;
+
+public class POST_BDDStyle {
+
+}
--- src/test/java/org/thetestingacademy/org/thetestingacademy/ex_03_RestAssured_TestNg_Advance/POST_BDDStyle.java
+++
@@ -1,5 +0,0 @@
-package org.thetestingacademy.org.thetestingacademy.ex_03_RestAssured_TestNg_Advance;
-
-public class POST_BDDStyle {
-
-}
